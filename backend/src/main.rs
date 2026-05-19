d4b00b87fa38735d8508fe6f035f428077d98186
d3lchan <yashchandel7@gmail.com>
2026-05-20 02:36:59 +0530

    harcoded parts update

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,13 +2,47 @@
 extern crate rocket;
 use rocket::serde::json::Json;
 use serde::Serialize;
-#[derive]
 
+#[derive(Serialize)]
+struct Part {
+id: i32,
+name: String,
+category: String,
+price_cents: i32,
+in_stock: bool,
+}
+#[get("/parts")]
+fn get_parts() -> Json<Vec<Part>> {
+    let parts = vec![
+        Part {
+            id: 1,
+            name:String::from("Vesrah Ceramic Brake Pads"),
+            category: String::from("Handling"),
+            price_cents: 3499,
+            in_stock: true,
+        },
+        Part {
+            id: 1,
+            name:String::from("LiquiMoly 10W40 1L engine oil"),
+            category: String::from("Engine"),
+            price_cents: 1299,
+            in_stock: true,
+        },
+        Part {
+            id: 1,
+            name:String::from("BMC Air Filter"),
+            category: String::from("Engine"),
+            price_cents: 8499,
+            in_stock: true,
+        },
+    ];
+    Json(parts)
+}
 #[get("/")]
 fn index() -> &'static str{
     "Pookies Garage API"
 }
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index,get_parts])
 }
